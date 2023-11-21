e67d8af9e31034e3116e0a8e174a70eb99545a53
Aleksander Lasek <aleksanderlasek01@gmail.com>
2023-11-21 18:38:18 +0000

    calls working

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const httpServer = express();
 const dialer = require('dialer').Dialer;
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+httpServer.use(bodyParser.json());
+httpServer.use(cors());
+httpServer.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+ next();
+});
 
 const config = {
  url: 'https://uni-call.fcc-online.pl',
@@ -21,3 +31,29 @@ const number2 = req.params.number2;
  dialer.call(number1,number2);
  res.json({success: true});
 })
+
+httpServer.post('/call/', async (req, res) => {
+    const number1 = req.body.number;
+    const number2 = '732008865' // tutaj dejemy swój numer
+     console.log('Dzwonie', number1, number2)
+    const bridge = await dialer.call(number1, number2);
+    let interval = setInterval(async () => {
+     let status = await bridge.getStatus();
+     console.log(status)
+     if (
+     status === "ANSWERED" ||
+     status === "FAILED" ||
+     status === "BUSY" ||
+     status === "NO ANSWER"
+     ) {
+     console.log("stop");
+     clearInterval(interval);
+     }
+     }, 2000);
+     res.json({ success: true });
+    })
+
+    httpServer.get('/status', async function (req, res) {
+        let status = await bridge.getStatus();
+         res.json({ id: '123', "status": status });
+        })
