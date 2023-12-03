cfe08b7f37007cad8c0b5b4670338c6b16de074d
Aleksander Lasek <aleksanderlasek01@gmail.com>
2023-12-03 19:44:36 +0000

    ringing and connected view timers

--- front/src/views/ConnectedView.vue
+++ front/src/views/ConnectedView.vue
@@ -1,7 +1,34 @@
 <template>
 <div class="box">
  <div class="text">
-    Trwa połączenie...
+    Trwa połączenie...<br>Czas rozmowy: {{ seconds }}s
  </div>
 </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      seconds: 0,
+      intervalId: null
+    };
+  },
+  mounted() {
+    this.startTimer();
+  },
+  unmounted() {
+    this.stopTimer();
+  },
+  methods: {
+    startTimer() {
+      this.intervalId = setInterval(() => {
+        this.seconds++;
+      }, 1000);
+    },
+    stopTimer() {
+      clearInterval(this.intervalId);
+    }
+  }
+};
+</script>
--- front/src/views/RingingView.vue
+++ front/src/views/RingingView.vue
@@ -1,7 +1,34 @@
 <template>
 <div class="box">
  <div class="text">
-   Zaraz nastąpi połączenie z konsultantem.
+   Zaraz nastąpi połączenie z konsultantem.<br>Oczekiwanie: {{ seconds }}s
  </div>
 </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      seconds: 0,
+      intervalId: null
+    };
+  },
+  mounted() {
+    this.startTimer();
+  },
+  unmounted() {
+    this.stopTimer();
+  },
+  methods: {
+    startTimer() {
+      this.intervalId = setInterval(() => {
+        this.seconds++;
+      }, 1000);
+    },
+    stopTimer() {
+      clearInterval(this.intervalId);
+    }
+  }
+};
+</script>
